8dd439a710ae6a7c2207c42c285a8cc2e468ce5e
Bruce Wilson <brucewilson1103@gmail.com>
2021-01-03 21:42:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+random_coin = random.randint(0,1)
+
+if random_coin ==1:
+  print('''
+  *************************************
+              _.-'~~`~~'-._
+         .'`  B   E   R  `'.
+        / I               T \;
+      /`       .-'~"-.       `\.
+     /; L     / `-    \     Y ;
+    ;        />  `.  -|        ;
+    |       /_     '-.__)       |
+    |        |-  _.' \ |        |
+    ;        `~~;     \\        ;
+     ;  INGODWE /      \\)P    ;
+      \  TRUST '.___.-'`"     /
+       `\                   /`
+         '._   1 9 9 7   _.'
+           `'-..,,,..-'`'
+                Heads
+   *********************************
+           ''')
+else:
+  print('''
+         _
+ .---.  / > .---,
+  <_  `'  `'  _>
+    <_/\  /\_>
+       /`'|
+      ".__." 
+      Tails
+           ''')
+
+
+
+
+
+
